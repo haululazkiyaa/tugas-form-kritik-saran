35fa3f929798b81408c76409108de687a295506d
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-09-04 00:20:38 +0700

    first commit

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,61 @@
+import useFeedback from "../hooks/useFeedback";
+
+const Feedback = () => {
+  const {
+    inputName,
+    setInputName,
+    inputFeedback,
+    setInputFeedback,
+    feedbackList,
+    submitFeedback,
+    likeFeedback,
+  } = useFeedback();
+
+  return (
+    <div className="mb-4">
+      <div>
+        <input
+          type="text"
+          placeholder="Nama"
+          value={inputName}
+          onChange={(e) => setInputName(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 mb-2"
+        />
+        <textarea
+          placeholder="Saran Anda..."
+          value={inputFeedback}
+          onChange={(e) => setInputFeedback(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+      </div>
+      <button
+        onClick={submitFeedback}
+        className="px-4 py-2 bg-teal-500 text-white rounded-md hover:bg-teal-600"
+      >
+        Kirim Saran
+      </button>
+      <div className="mt-6">
+        {feedbackList.map((item, index) => (
+          <div
+            key={index}
+            className="p-4 border border-gray-200 rounded-md mb-4 flex justify-between items-center"
+          >
+            <div>
+              <p className="font-bold">{item.name}</p>
+              <p>{item.feedback}</p>
+            </div>
+            <button
+              onClick={() => likeFeedback(index)}
+              className="px-3 py-1 bg-teal-500 text-white rounded-md hover:bg-teal-600"
+            >
+              <i className="fa-solid fa-thumbs-up me-1"></i> {item.likes}{" "}
+              {item.likes > 1 ? "Likes" : "Like"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Feedback;
---
+++ src/hooks/useFeedback.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+const useFeedback = () => {
+  const [inputName, setInputName] = useState("");
+  const [inputFeedback, setInputFeedback] = useState("");
+  const [feedbackList, setFeedbackList] = useState([]);
+
+  const submitFeedback = () => {
+    const newFeedbackList = [
+      ...feedbackList,
+      { name: inputName, feedback: inputFeedback, likes: 0 },
+    ];
+
+    setFeedbackList(newFeedbackList);
+    localStorage.setItem("feedbackList", JSON.stringify(newFeedbackList));
+
+    setInputName("");
+    setInputFeedback("");
+  };
+
+  const likeFeedback = (index) => {
+    const newFeedbackList = [...feedbackList];
+    newFeedbackList[index].likes += 1;
+    setFeedbackList(newFeedbackList);
+    localStorage.setItem("feedbackList", JSON.stringify(newFeedbackList));
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("feedbackList") === null) {
+      const initialFeedback = [
+        {
+          name: "Budi Susilo Maulana",
+          feedback: "Harga Produknya supaya bisa lebih murah dari tempat lain",
+          likes: 0,
+        },
+      ];
+      localStorage.setItem("feedbackList", JSON.stringify(initialFeedback));
+    }
+
+    setFeedbackList(JSON.parse(localStorage.getItem("feedbackList")));
+  }, []);
+
+  return {
+    inputName,
+    setInputName,
+    inputFeedback,
+    setInputFeedback,
+    feedbackList,
+    submitFeedback,
+    likeFeedback,
+  };
+};
+
+export default useFeedback;
